91874682727d2e59166c58e6ebceb8629cda1e76
Victor Abiscula <victorabiscula@live.com>
2026-05-03 17:41:11 -0300

    feat: Estrutura inicial para teste da api

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function getDashboard() {
+  const res = await fetch(`${API_URL}/api/dashboard`);
+  const data = await res.json();
+  return data;
+}
