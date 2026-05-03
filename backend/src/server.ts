29775ba1f47611759a366be064e9cb4fa81c9c51
Victor Abiscula <victorabiscula@live.com>
2026-05-03 17:44:13 -0300

    feat: adiciona suporte a CORS para integração com frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
 const PORT = 3000;
 
 app.use("/api", routes);
