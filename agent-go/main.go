731c2dad683d1000f7a7575907fda40f7d506c91
Victor Abiscula <victorabiscula@live.com>
2026-05-10 17:53:22 -0300

    feat: Cria novo projeto de agent em golang

---
+++ agent-go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"agent-go/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	routes.RegisterDockerRoutes(router)
+
+	router.Run(":3010")
+}
