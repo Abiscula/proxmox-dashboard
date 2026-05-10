efff5b2e93313b73d664bbbac8d08f302949fb09
Victor Abiscula <victorabiscula@live.com>
2026-05-10 17:51:49 -0300

    feat: Adiciona type DockerContainer

---
+++ agent-go/types/docker_container.go
@@ -0,0 +1,9 @@
+package types
+
+type DockerContainer struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Image  string `json:"image"`
+	State  string `json:"state"`
+	Status string `json:"status"`
+}
